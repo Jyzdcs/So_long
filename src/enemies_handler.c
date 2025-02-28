00f1b4d449650dfb6cac036bfddd47cad41abe01
Jyzdcs <ky.claudant@gmail.com>
2025-02-28 14:46:47 +0100

    must to be corrected

--- src/enemies_handler.c
+++ src/enemies_handler.c
@@ -6,7 +6,7 @@
 /*   By: kclaudan <kclaudan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/27 14:30:22 by kclaudan          #+#    #+#             */
-/*   Updated: 2025/02/28 13:09:21 by kclaudan         ###   ########.fr       */
+/*   Updated: 2025/02/28 13:45:38 by kclaudan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,7 +101,7 @@ int	update_enemies(t_game *game)
 	int			i;
 
 	update_counter++;
-	if (update_counter % 2000 != 0)
+	if (update_counter % 30000 != 0)
 		return (1);
 	i = 0;
 	while (i < game->index)
